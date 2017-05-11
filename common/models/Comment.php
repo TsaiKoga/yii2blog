66f764b9306a168d2078df8893aa855bd2629f1d
TsaiKoga <969024089@qq.com>
2017-05-11 15:57:46 +0800

    Gii create models

---
+++ common/models/Comment.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "comment".
+ *
+ * @property integer $id
+ * @property string $content
+ * @property string $create_time
+ * @property string $email
+ * @property string $url
+ * @property integer $post_id
+ * @property integer $user_id
+ *
+ * @property Post $post
+ * @property User $user
+ */
+class Comment extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'comment';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['content'], 'string'],
+            [['create_time'], 'safe'],
+            [['post_id', 'user_id'], 'required'],
+            [['post_id', 'user_id'], 'integer'],
+            [['email', 'url'], 'string', 'max' => 255],
+            [['post_id'], 'exist', 'skipOnError' => true, 'targetClass' => Post::className(), 'targetAttribute' => ['post_id' => 'id']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+            [['status'], 'exist', 'skipOnError' => true, 'targetClass' => Commentstatus::className(), 'targetAttribute' => ['status' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'content' => 'Content',
+            'create_time' => 'Create Time',
+            'email' => 'Email',
+            'url' => 'Url',
+            'post_id' => 'Post ID',
+            'user_id' => 'User ID',
+            'status' => 'Status',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPost()
+    {
+        return $this->hasOne(Post::className(), ['id' => 'post_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+}
---
+++ common/models/Post.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $content
+ * @property string $tags
+ * @property string $create_time
+ * @property string $update_time
+ * @property integer $author_id
+ * @property integer $status
+ *
+ * @property User $author_id
+ * @property Poststatus $status
+ *
+ * @property Comment[] $comments
+ * @property Poststatus $status0
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+          [['title', 'content', 'status', 'author_id'], 'required'],
+          [['content', 'tags'], 'string'],
+          [['status', 'author_id'], 'integer'],
+          [['title'], 'string', 'max' => 128],
+          [['author_id'], 'exist', 'skipOnError' => true, 'targetClass' => Adminuser::className(), 'targetAttribute' => ['author_id' => 'id']],
+          [['status'], 'exist', 'skipOnError' => true, 'targetClass' => Poststatus::className(), 'targetAttribute' => ['status' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => '标题',
+            'content' => '内容',
+            'tags' => '标签',
+            'create_time' => '创建时间',
+            'update_time' => '修改时间',
+            'author_id' => '作者',
+            'status' => '状态',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getComments()
+    {
+        return $this->hasMany(Comment::className(), ['post_id' => 'id']);
+    }
+
+    // 这里'id'=>'name' id是指poststatus的id指向post的status字段
+    public function getStatus0()
+    {
+      return $this->hasOne(Poststatus::className(), ['id' => 'status']);
+    }
+
+    public function getAuthor() {
+      return $this->hasOne(User::className(), ['id' => 'author_id']);
+    }
+}
